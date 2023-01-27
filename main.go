342a8765df7f26704ff9ade074a219e79139a551
glassyeyedfish <glassyeyedfish@gmail.com>
2023-01-27 11:38:14 -0500

    Finished basics

--- main.go
+++ main.go
@@ -8,20 +8,21 @@ import (
 	"os"
 )
 
-const w, h = 256, 256
-const hw, hh = w / 2, h / 2
-const step = 5
+const IMAGE_SIZE = 128
+const START_X, START_Y = IMAGE_SIZE / 2, IMAGE_SIZE / 2
+const STEP = 5
+const MAX_ITERATIONS = 7
 
 const (
-	UP    = iota
-	DOWN  = iota
-	RIGHT = iota
-	LEFT  = iota
+	UP    = 0
+	RIGHT = 1
+	DOWN  = 2
+	LEFT  = 3
 )
 
 func Clear(img *image.NRGBA) {
-	for y := 0; y < h; y++ {
-		for x := 0; x < w; x++ {
+	for y := 0; y < IMAGE_SIZE; y++ {
+		for x := 0; x < IMAGE_SIZE; x++ {
 			img.Set(x, y, color.NRGBA{
 				R: 0,
 				G: 0,
@@ -65,17 +66,44 @@ func DrawLine(img *image.NRGBA, x1 int, y1 int, x2 int, y2 int) {
 func main() {
 
 	// Create a colored image of the given width and height.
-	img := image.NewNRGBA(image.Rect(0, 0, w, h))
+	img := image.NewNRGBA(image.Rect(0, 0, IMAGE_SIZE, IMAGE_SIZE))
 
-	// Set cursor to the middle of the image
-	cx, cy := hw, hh
+	cursor_x, cursor_y := START_X, START_Y
+	index := 0
 
 	// Fill the image with black pixels
 	Clear(img)
 
-	DrawLine(img, cx, cy, cx, cy+step)
-	cy = cy + step
-	DrawLine(img, cx, cy, cx+step, cy)
+	// UP -> RIGHT -> DOWN -> LEFT -> UP
+	// Drawing instructions
+	instructions := []int{UP}
+
+	for iter := 1; iter <= MAX_ITERATIONS; iter++ {
+
+		// Follow the instructions
+		for ; index < len(instructions); index++ {
+			switch instructions[index] {
+			case UP:
+				DrawLine(img, cursor_x, cursor_y, cursor_x, cursor_y-STEP)
+				cursor_y -= STEP
+			case DOWN:
+				DrawLine(img, cursor_x, cursor_y, cursor_x, cursor_y+STEP)
+				cursor_y += STEP
+			case LEFT:
+				DrawLine(img, cursor_x, cursor_y, cursor_x-STEP, cursor_y)
+				cursor_x -= STEP
+			case RIGHT:
+				DrawLine(img, cursor_x, cursor_y, cursor_x+STEP, cursor_y)
+				cursor_x += STEP
+			}
+		}
+
+		// Work backwards to generate next set of instructions
+		for i := index - 1; i >= 0; i-- {
+			n := (instructions[i] + 1) % 4
+			instructions = append(instructions, n)
+		}
+	}
 
 	// Save it to the image file
 	f, err := os.Create("dragon.png")
